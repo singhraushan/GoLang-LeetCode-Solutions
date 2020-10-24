3bdc6f700767c9a82acffa6a2f5e80d28f8c98fb
singhraushan <singhraushan500@gmail.com>
2020-10-24 14:08:35 +0530

    GoLang: LeetCode Q.1

---
+++ 7.ReverseInteger/7.ReverseInteger.go
@@ -0,0 +1,24 @@
+package main
+
+import ("fmt"
+"math"
+)
+
+func main(){
+
+	fmt.Printf("max int32: %d\n", math.MaxInt32)
+	fmt.Printf("min int32: %d\n", math.MinInt32)
+	fmt.Printf("Reverse value is %d.", reverse(1534236469))
+	
+}
+func reverse(x int) int {
+	r, min,max := 0, math.MinInt32, math.MaxInt32
+	for x!=0 {
+	 r = r*10 + x%10
+	 x /= 10
+	}
+	if r>max || r<min {
+		return 0
+	}
+    return r
+}
